cae68c93a408e9c4246068d35ac8605dc658fe0e
Howard Sun <komugi@gmail.com>
2024-03-06 13:23:53 -0500

    Working React App without CRA Working webpack
    Changes to be committed:
    	new file:   .gitignore
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   src/Components/App.js
    	new file:   src/index.html
    	new file:   src/index.js
    	new file:   webpack.config.js

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+        <h2>this is the second try</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: '/src/index.js',
+  output: {
+    path: path.resolve(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Tells devServer where to serve content from
+      publicPath: '/',
+    },
+    compress: true,
+    port: 8080,
+    historyApiFallback: true, // Useful for single-page applications
+    open: true, // Automatically open the browser
+    // proxy: [{ context: ['/api'], target: 'http://localhost:3000' }],
+  },
+};
